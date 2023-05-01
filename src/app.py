7655ebb170273b599ebcef71301d0c262e414a33
mjconl6291 <mjconl6291@gmail.com>
2023-05-01 00:01:46 -0500

    troubleshooting

--- src/app.py
+++ src/app.py
@@ -9,14 +9,14 @@ import sys
 def handler(event,context):
 
     body = event['Records'][0]['Sns']['Message']
-    # body = json.loads(body)
+    body = json.loads(body)
     # queue_id = body['queue_id']
-    # master_id = body['master_id']
-    # mrn = body['mrn']
-    # event_type = body['event']
-    print('event ', event)
-    print('body ', body)
-    # post_to_mahler(queue_id, master_id, mrn, event_type)
+    master_id = body['master_id']
+    mrn = body['mrn']
+    event_type = body['event']
+    # print('event ', event)
+    # print('body ', body)
+    post_to_mahler(0, master_id, mrn, event_type)
 
 
 ssm = boto3.client('ssm',  aws_access_key_id=os.environ['KEY'], aws_secret_access_key=os.environ['SECRET'],  region_name='us-east-2')
