2b23deef45863ace9852dcc2860f9b9afb322c32
mjconl6291 <mjconl6291@gmail.com>
2023-05-10 14:55:18 -0500

    added event function, reworked handler function to logically call the functions by event_type

--- src/app.py
+++ src/app.py
@@ -7,23 +7,22 @@ import sys
 
 
 def handler(event,context):
-
     body = event['Records'][0]['Sns']['Message']
     body = json.loads(body)
-    # queue_id = body['queue_id']
     master_id = body['master_id']
     mrn = body['mrn']
     event_type = body['event']
-    # print('event ', event)
-    # print('body ', body)
-    post_to_mahler(0, master_id, mrn, event_type)
+    if event_type == 'new':
+        post_new_patient(master_id, mrn)
+    elif event_type == 'update':
+        post_demographics(master_id, mrn)
+    elif event_type == 'event':
+        post_event(master_id, mrn)
 
 
 ssm = boto3.client('ssm',  aws_access_key_id=os.environ['KEY'], aws_secret_access_key=os.environ['SECRET'],  region_name='us-east-2')
 param = ssm.get_parameter(Name='uck-etl-db-prod-masterdata', WithDecryption=True )
 db_request = json.loads(param['Parameter']['Value']) 
-# env = Environment(extensions=['jinja2.ext.loopcontrols'])
-# env.loader = FileSystemLoader(os.path.dirname(__file__) + '/templates')
 
 def masterdata_conn():
     hostname = db_request['host']
@@ -34,45 +33,46 @@ def masterdata_conn():
     conn = psycopg2.connect(host=hostname,user=dbusername,port=portno,password=dbpassword,dbname=dbname)
     return conn
 
+url = os.environ['URL']
+headers= {'Content-Type': 'application/x-www-form-urlencoded'}
 
-def post_to_mahler(queue_id, master_id, mrn, event_type):
+def post_new_patient(master_id, mrn):
     _targetconnection = masterdata_conn()
     cur = _targetconnection.cursor()
-    url = os.environ['URL']
-    headers= {'Content-Type': 'application/x-www-form-urlencoded'}
+    get_new_patient_call = f"call public.mahler_get_new_patient('{master_id}', '{{}}')"
+    try:
+        cur.execute(get_new_patient_call)
+        _targetconnection.commit()
+    except Exception as e:
+        print(f"Error executing upsert_mrn_call: {str(e)}")         
+    mahler_post_payload = cur.fetchone()
+    mahler_post_payload = json.dumps(mahler_post_payload)
+    data = json.loads(mahler_post_payload)[0]
+        # add username and key from env variables
+    data['username'] = os.environ['USERNAME1']
+    data['key'] = os.environ['MAHLER_KEY']
+    data['action'] = 'api_initial_client_register'
+    print(json.dumps(data))
+    response = requests.post(url, headers=headers, data=data)
+    print(response.text)
+    response = json.loads(response.text)
+    mahler_client_id = response['client_id']
+    print(mahler_client_id) 
+    upsert_mrn_call = f"call public.mahler_upsert_mrn('{master_id}', {mahler_client_id});"
+    print(upsert_mrn_call)
+    try:
+        cur.execute(upsert_mrn_call)
+        _targetconnection.commit()
+    except Exception as e:
+        print(f"Error executing upsert_mrn_call: {str(e)}")
+    mrn = mahler_client_id
+    cur.close()
+    _targetconnection.close()
+    post_demographics(master_id, mrn)
 
-    if event_type == 'new':
-         get_new_patient_call = f"call public.mahler_get_new_patient('{master_id}', '{{}}')"
-         try:
-            cur.execute(get_new_patient_call)
-            _targetconnection.commit()
-         except Exception as e:
-            print(f"Error executing upsert_mrn_call: {str(e)}")         
-         mahler_post_payload = cur.fetchone()
-         mahler_post_payload = json.dumps(mahler_post_payload)
-         data = json.loads(mahler_post_payload)[0]
-         # add username and key from env variables
-         data['username'] = os.environ['USERNAME1']
-         data['key'] = os.environ['MAHLER_KEY']
-         data['action'] = 'api_initial_client_register'
-         print(json.dumps(data))
-         response = requests.post(url, headers=headers, data=data)
-         print(response.text)
-         response = json.loads(response.text)
-         mahler_client_id = response['client_id']
-         print(mahler_client_id) 
-         upsert_mrn_call = f"call public.mahler_upsert_mrn('{master_id}', {mahler_client_id});"
-         print(upsert_mrn_call)
-         try:
-            cur.execute(upsert_mrn_call)
-            _targetconnection.commit()
-         except Exception as e:
-            print(f"Error executing upsert_mrn_call: {str(e)}")
-         mrn = mahler_client_id
-    else:
-        #placeholder assnmnt
-        print(mrn)
-
+def post_demographics(master_id, mrn):
+    _targetconnection = masterdata_conn()
+    cur = _targetconnection.cursor()
     get_demographics_call = f"call public.mahler_get_demographics('{master_id}', {mrn}, '{{}}');"
     try:
         cur.execute(get_demographics_call)
@@ -80,13 +80,32 @@ def post_to_mahler(queue_id, master_id, mrn, event_type):
     except Exception as e:
         print(f"Error executing upsert_mrn_call: {str(e)}")
     mahler_full_demo_payload = json.dumps(cur.fetchone())
-    
-    print(mahler_full_demo_payload)
-    
+    print(mahler_full_demo_payload) 
     demo_data = json.loads(mahler_full_demo_payload)[0]   
     demo_data['username'] = os.environ['USERNAME1']
     demo_data['key'] = os.environ['MAHLER_KEY']
     demo_data['action'] = 'api_update_client_information'     
     demo_response = requests.post(url, headers=headers, data=demo_data)
     print(demo_response.text)
+    cur.close()
+    _targetconnection.close()
 
+def post_event(master_id, mrn):
+    _targetconnection = masterdata_conn()
+    cur = _targetconnection.cursor()
+    get_event_call = f"call public.mahler_get_event('{master_id}',{mrn},'{{}}');"
+    try:
+        cur.execute(get_event_call)
+        _targetconnection.commit()
+    except Exception as e:
+        print(f"Error executing get_event_call: {str(e)}")
+    mahler_event_payload = json.dumps(cur.fetchone())
+    print(mahler_event_payload)
+    event_data = json.loads(mahler_event_payload)[0]   
+    event_data['username'] = os.environ['USERNAME1']
+    event_data['key'] = os.environ['MAHLER_KEY']
+    event_data['action'] = 'api_schedule_event'     
+    event_response = requests.post(url, headers=headers, data=event_data)
+    print(event_response.text)
+    cur.close()
+    _targetconnection.close()
